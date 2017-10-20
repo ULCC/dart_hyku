a69727dc89b942a5a393718a575e4127c37f60ec
Julie Allinson <julie.allinson@london.ac.uk>
2017-10-20 18:59:33 +0100

    configure rails_tmp and sprockets

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,13 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Relocate RAILS_TMP
+  config.assets.configure do |env|
+    env.cache = Sprockets::Cache::FileStore.new(
+        ENV['RAILS_TMP'] + '/cache/assets'
+    )
+  end
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
